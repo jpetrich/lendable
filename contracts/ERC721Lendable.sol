e7f97206babf8c25440d1b73327f6b13503d0192
Joe Petrich <petrich89@gmail.com>
2021-12-03 17:30:15 -0500

    Update logic to disallow transfers of borrowed tokens except back to the lender. Also provide a method to get the lenderOf a given token, similar to ownerOf. Update the method to transfer the lender role to one that transfers ownership and lender-role at once to make the logic more obvious.

--- contracts/ERC721Lendable.sol
+++ contracts/ERC721Lendable.sol
@@ -22,6 +22,26 @@ contract ERC721Lendable is ERC721 {
     }
 
     /**
+     * @dev Prevents the borrower from transferring a token except back to the lender.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) virtual public override {
+        require(from == _lenders[tokenId] || to == _lenders[tokenId], 'Transfer of a borrowed token except to the lender is not allowed.');
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+
+    /**
+     * @dev Prevents the borrower from transferring a token except back to the lender.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        require(from == _lenders[tokenId] || to == _lenders[tokenId], 'Transfer of a borrowed token except to the lender is not allowed.');
+        super.transferFrom(from, to, tokenId);
+    }
+
+    /**
     * @dev Mints a token to the specified address, initially setting the lender
     to the `to` address for `tokenId`
      */
@@ -30,10 +50,21 @@ contract ERC721Lendable is ERC721 {
         _lenders[tokenId] = to;
     }
 
-    function transferLenderRecordFrom(address from, address to, uint256 tokenId) public {
+    /**
+     * @dev Transfers ownership and the lender role to the `to` address.
+     */
+    function transferWithFullRightsFrom(address from, address to, uint256 tokenId) public {
         require(_lenders[tokenId] == from, 'Token is not registered to the specified lender');
         require(msg.sender == from, 'Sender is not the specified lender');
+        transferFrom(from, to, tokenId);
         _lenders[tokenId] = to;
+    }
+
+    /**
+     * @dev Find the lender of the specified token.
+     */
+    function lenderOf(uint256 tokenId) public view returns (address) {
+        return _lenders[tokenId];
     }
 
     /**
--- test/ERC721Lendable.test.js
+++ test/ERC721Lendable.test.js
@@ -50,16 +50,26 @@ describe("ERC721Lendable Test Suite", function() {
             await expect(balance.toNumber()).to.equal(1);
         });
 
-        it("should be able to transfer an ERC721 token as the current borrower", async function() {
+        it("should be able to transfer an ERC721 token as the current borrower back to the lender", async function() {
             // Mint to the lender
             await this.ERC721Mock.connect(this.deployer).mint(this.lender.address, this.tokenId);
             // Transfer from lender to borrower
             await this.ERC721Mock.connect(this.lender).transferFrom(this.lender.address, this.borrower.address, this.tokenId);
 
-            // As the borrower, transfer the token to a third party
-            await this.ERC721Mock.connect(this.borrower).transferFrom(this.borrower.address, this.thirdParty.address, this.tokenId);
-            let balance = await this.ERC721Mock.balanceOf(this.thirdParty.address);
+            // Transfer the token back to the lender from the borrower
+            await this.ERC721Mock.connect(this.borrower).transferFrom(this.borrower.address, this.lender.address, this.tokenId);
+            let balance = await this.ERC721Mock.balanceOf(this.lender.address);
             await expect(balance.toNumber()).to.equal(1);
+        });
+
+        it("should not be able to transfer an ERC721 token as the current borrower to a third party", async function() {
+            // Mint to the lender
+            await this.ERC721Mock.connect(this.deployer).mint(this.lender.address, this.tokenId);
+            // Transfer from lender to borrower
+            await this.ERC721Mock.connect(this.lender).transferFrom(this.lender.address, this.borrower.address, this.tokenId);
+
+            // As the borrower, attempt to transfer the token to a third party
+            await expect( this.ERC721Mock.connect(this.borrower).transferFrom(this.borrower.address, this.thirdParty.address, this.tokenId)).to.be.rejectedWith('Transfer of a borrowed token except to the lender is not allowed.');
         });
 
         it("should not be possible to transfer an ERC721 token if not the owner or lender", async function() {
@@ -68,7 +78,18 @@ describe("ERC721Lendable Test Suite", function() {
             // Transfer from lender to borrower
             await this.ERC721Mock.connect(this.lender).transferFrom(this.lender.address, this.borrower.address, this.tokenId);
 
-            await expect(this.ERC721Mock.connect(this.thirdParty).transferFrom(this.borrower.address, this.thirdParty.address, this.tokenId)).to.be.rejectedWith('ERC721: transfer caller is not owner nor approved');
+            await expect(this.ERC721Mock.connect(this.thirdParty).transferFrom(this.borrower.address, this.thirdParty.address, this.tokenId)).to.be.rejectedWith('Transfer of a borrowed token except to the lender is not allowed.');
+        });
+
+        it("should be able to transfer owner and lender role of an ERC721 token as the current lender", async function() {
+            // Mint to the lender
+            await this.ERC721Mock.connect(this.deployer).mint(this.lender.address, this.tokenId);
+            // Transfer from lender to borrower
+            await this.ERC721Mock.connect(this.lender).transferWithFullRightsFrom(this.lender.address, this.thirdParty.address, this.tokenId);
+            let balance = await this.ERC721Mock.balanceOf(this.thirdParty.address);
+            await expect(balance.toNumber()).to.equal(1);
+            let tokenLenderAddress = await this.ERC721Mock.lenderOf(this.tokenId);
+            await expect(tokenLenderAddress).to.equal(this.thirdParty.address);
         });
     })
 });
